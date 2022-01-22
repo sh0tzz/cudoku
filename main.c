9b6bc832be0562c9f29092e73de6f6ba45fa9b23
sh0tzz <lubranier@gmail.com>
2022-01-22 14:43:07 +0100

    render grid

--- main.c
+++ main.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
+void renderGrid(int *p_grid) {
+	int i, j;
+	for (i = 0; i < 9; i++) {
+		for (j = 0; j < 9; j++) {
+			printf(" %d ", *(p_grid+i*9+j));
+			if (i+1 % 3 == 0) {
+				printf("|");
+			}
+		}
+		printf("\n");
+	}	
+}
+
 int main()
 {
-	printf("Hello World\n");
+	int i, j;
+	int grid[9][9];
+	for (i = 0; i < 9; i++) {
+		for (j = 0; j < 9; j++) {
+			grid[i][j] = 0;
+		}
+	}
+	renderGrid(&grid[0][0]);
 	return 0;
 }
